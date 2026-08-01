04d3f2fe757daadfaee9c523110cde774269fa62
Riticaa <riticaa@github.com>
2026-08-01 17:29:29 +0530

    Fix super resolution model path

--- backend/enhancement/super_resolution.py
+++ backend/enhancement/super_resolution.py
@@ -2,21 +2,30 @@ import cv2
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, "models", "FSRCNN_x4.pb")
 
+MODEL_PATH = os.path.abspath(
+    os.path.join(BASE_DIR, "..", "models", "FSRCNN_x4.pb")
+)
+
+print("MODEL PATH:", MODEL_PATH)
+print("MODEL EXISTS:", os.path.exists(MODEL_PATH))
 sr = None
 
 def get_model():
+    
     global sr
 
     if sr is None:
         print("Loading Super Resolution model...")
-        sr = cv2.dnn_superres.DnnSuperResImpl_create()
-        sr.readModel(MODEL_PATH)
-        sr.setModel("fsrcnn", 4)
-        print("Model loaded successfully!")
 
-    return sr
+        try:
+            sr = cv2.dnn_superres.DnnSuperResImpl_create()
+            sr.readModel(MODEL_PATH)
+            sr.setModel("fsrcnn", 4)
+            print("Model loaded successfully!")
+        except Exception as e:
+            print("ERROR:", e)
+            raise
 
 
 def enhance_resolution(image):
